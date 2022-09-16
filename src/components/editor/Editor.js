fe42c473dfee68692aac7f2527c9c61a4ca85b15
sh-2000 <sjdhasan46@gmail.com>
2022-09-17 03:20:04 +1000

    canvas

--- src/components/editor/Editor.js
+++ src/components/editor/Editor.js
@@ -1,33 +1,57 @@
-import { useEffect, useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import { useParams } from "react-router-dom";
-import Navbar from "../Navbar";
+import { LeftPanel, RightPanel } from "./SidePanel";
+import Canvas from "./Canvas";
+import { Box } from "@mui/material";
+import MenuBar from "./MenuBar";
 
 const Editor = () => {
   const { projectID } = useParams();
   const [project, setProject] = useState("");
+  const [categories, setCategories] = useState("");
+  const [icons, setIcons] = useState("");
+
+  const boxRef = useRef();
 
   useEffect(() => {
     const getProject = async () => {
-      const resData = await fetchProject();
+      const resData = await fetchData(
+        `http://localhost:3001/projects/${projectID}`
+      );
       setProject(resData);
     };
     getProject();
+    const getCategories = async () => {
+      const resData = await fetchData("http://localhost:3001/categories");
+      setCategories(resData);
+    };
+    getCategories();
+    const getIcons = async () => {
+      const resData = await fetchData("http://localhost:3001/icons");
+      setIcons(resData);
+    };
+    getIcons();
   }, []);
 
-  console.log(project);
-
-  const fetchProject = async () => {
-    const res = await fetch(`http://localhost:3001/projects/${projectID}`);
+  const fetchData = async (req) => {
+    const res = await fetch(req);
     const data = await res.json();
     return data;
   };
 
   return (
     <>
-      <Navbar />
-      <h2>Edit Page</h2>
-      <h4>Editing {project.title} </h4>
-      <img src={project.schema} />
+      <MenuBar project={project} />
+      <LeftPanel icons={icons} categories={categories} />
+      <RightPanel />
+      <Box
+        sx={{
+          mx: "16%",
+          my: "1%",
+        }}
+      >
+        <Canvas url={project.schema} />
+      </Box>
     </>
   );
 };
