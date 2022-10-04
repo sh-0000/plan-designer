874e59ed9ebfa5e75812d246990692f916fb4fa3
sh-2000 <sjdhasan46@gmail.com>
2022-10-04 18:58:45 +1100

    updated the readme file

--- src/components/IconList.js
+++ src/components/IconList.js
@@ -12,8 +12,17 @@ const IconList = ({ icons }) => {
       {icons.length > 0 ? ( //Show a no matches msg if icons.length < 1
         <ImageList sx={{ width: "100%", height: "100%" }} cols={5}>
           {icons.map((icon) => (
-            <ImageListItem key={icon.id}>
-              <img src={icon.img} alt={icon.name} loading="lazy" />
+            <ImageListItem key={icon.id} sx={{ paddingInline: "1rem" }}>
+              <img
+                style={{
+                  border: "1px solid hsl(0 0% 0% / 0.1)",
+                  borderRadius: "10px",
+                  padding: '10px'
+                }}
+                src={icon.img}
+                alt={icon.name}
+                loading="lazy"
+              />
               <ImageListItemBar
                 title={icon.name}
                 subtitle={icon.category}
